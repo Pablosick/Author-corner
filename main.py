19946f11e1e9aa4ed00ab95f0447d60a10ba629e
Pablos1ck <bochkarevpasha18@gmail.com>
2023-02-26 02:39:27 +0400

    authorization

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'zlGK5teFsXX4e3jfyO'
@@ -14,10 +14,6 @@ def index():
     return render_template('index.html', title='Backend на Flask', menu=menu)
 
 
-@app.route('/about')
-def about():
-    return '<button><h1>Кнопка</button><h1>'
-
 @app.route('/contact', methods=["POST", "GET"]) #Если не указать метод POST, то на кнопку отправить будет ошибка 405. Сервер получает запрос, но не может его реализовать
 def contact():
     if request.method == "POST":
@@ -28,6 +24,20 @@ def contact():
 
     return render_template('contact.html', title='Обратная связь', menu=menu)
 
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'selfedu' and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', title='Авторизация', menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
